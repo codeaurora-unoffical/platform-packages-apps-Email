e44d9b1b4c4e6bb8ca44ac9673ba02f202657264
kaiyiz <kaiyiz@codeaurora.org>
2013-03-12 11:07:27 +0800

    Email: Update the folders' name when system language change
    The folder names of "inbox" "draft" and "sent" was initialized when the
    FolderProperties class was created. After device language changes, the
    instance will not be recreated. As a result, the folders display name will
    not change.
    
    To solve this problem, add logic in FolderProperties to reload the special
    folder names when device locale has been changed.
    
    CRs-Fixed: 371600
    
    Change-Id: I625244cc928758e51367f4f305dffbba852dd28f

--- src/com/android/email/FolderProperties.java
+++ src/com/android/email/FolderProperties.java
@@ -26,6 +26,7 @@ import android.content.res.TypedArray;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 
+import java.util.Locale;
 
 // TODO When the UI is settled, cache all strings/drawables
 /**
@@ -42,22 +43,20 @@ public class FolderProperties {
     private static FolderProperties sInstance;
 
     private final Context mContext;
-
     // Caches for frequently accessed resources.
-    private final String[] mSpecialMailbox;
+    private String[] mSpecialMailbox;
+
     private final TypedArray mSpecialMailboxDrawable;
     private final Drawable mSummaryStarredMailboxDrawable;
     private final Drawable mSummaryCombinedInboxDrawable;
 
+    private Locale mLocale;
+
     private FolderProperties(Context context) {
         mContext = context.getApplicationContext();
-        mSpecialMailbox = context.getResources().getStringArray(R.array.mailbox_display_names);
-        for (int i = 0; i < mSpecialMailbox.length; ++i) {
-            if ("".equals(mSpecialMailbox[i])) {
-                // there is no localized name, so use the display name from the server
-                mSpecialMailbox[i] = null;
-            }
-        }
+
+        updateMailboxName();
+
         mSpecialMailboxDrawable =
             context.getResources().obtainTypedArray(R.array.mailbox_display_icons);
         mSummaryStarredMailboxDrawable =
@@ -98,6 +97,8 @@ public class FolderProperties {
      */
     private String getDisplayName(int type, long mailboxId) {
         String name = getCombinedMailboxName(mailboxId);
+
+        updateMailboxName();
 
         if ((name == null) && (type < mSpecialMailbox.length)) {
             name = mSpecialMailbox[type];
@@ -230,5 +231,24 @@ public class FolderProperties {
         }
         return null; // No icon
     }
+
+    /**
+     * Reload the special folder names when device locale has been changed
+     */
+    private void updateMailboxName() {
+        Locale l = Locale.getDefault();
+        if ((mLocale == null) || !(l.equals(mLocale))) {
+            mLocale = l;
+            mSpecialMailbox = mContext.getResources().getStringArray(
+                    R.array.mailbox_display_names);
+            for (int i = 0; i < mSpecialMailbox.length; ++i) {
+                if ("".equals(mSpecialMailbox[i])) {
+                    // there is no localized name, so use the display name from the server
+                    mSpecialMailbox[i] = null;
+                }
+            }
+        }
+    }
+
 }
 
